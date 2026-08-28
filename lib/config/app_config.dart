b1a5e5837590cb664080869aa25a6e81c9df4ee1
toptighty-gmail <toptighty@gmail.com>
2026-08-28 12:06:23 +0100

    chore(release): bump version to 1.1.8+19

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String version = 'v1.1.7';
+  static const String version = 'v1.1.8';
   static const String appName = 'RFU HUB';
   static const String appSubTitle = 'Fixtures & Standings';
 }

ffbc8e5e0f2c6bccb4945ae4b7e3ad50e974c032
toptighty-gmail <toptighty@gmail.com>
2026-08-29 16:18:46 +0100

    fix(divisions): align Counties 2 Tribute Devon lineup and remove promoted Torquay Athletic & bump version to 1.1.36+47

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String version = 'v1.1.35';
+  static const String version = 'v1.1.36';
   static const String appName = 'RFU HUB';
   static const String appSubTitle = 'Fixtures & Standings';
 }
